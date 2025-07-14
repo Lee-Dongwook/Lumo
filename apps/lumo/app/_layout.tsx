72ad365af630a6a017e1865db77b34d9669a0b17
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-07-14 20:23:28 +0900

    feat: i18n

--- apps/lumo/app/_layout.tsx
+++ apps/lumo/app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from 'expo-router'
 import { QueryProvider } from 'shared'
+import { I18nextProvider } from 'react-i18next'
+import i18n from '@/i18n/i18n'
 
 export default function Layout() {
   return (
     <QueryProvider>
-      <Stack />
+      <I18nextProvider i18n={i18n}>
+        <Stack />
+      </I18nextProvider>
     </QueryProvider>
   )
 }
